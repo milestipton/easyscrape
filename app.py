a7370c672b30f28cc4e522c41cf69ba5c2c05372
milestipton <milestipton10@gmail.com>
2022-11-01 10:01:44 -0700

    Updating Search

--- app.py
+++ app.py
@@ -6,13 +6,32 @@ newsapi = NewsApiClient(api_key="20aa9cbb2d4d4735a0d27202348e50c7")
 
 app = Flask(__name__)
 
+def find_sources_and_domains():
+    all_sources = newsapi.get_sources()['sources']
+    sources = []
+    domains = []
+    for source in all_sources:
+        id = source['id']
+        domain = source['url'].replace("http://", "")
+        domain = domain.replace("https://", "")
+        domain = domain.replace("www.", "")
+        slash = domain.find('/')
+        if slash != -1:
+            domain = domain[:slash]
+        sources.append(id)
+        domains.append(domain)
+    sources = ", ".join(sources)
+    domains = ", ".join(domains)
+    return sources, domains 
+
 @app.route('/', methods=['POST', 'GET'])
 def home():
     if request.method == "POST":
         search = request.form['search']
         all_news = newsapi.get_everything(
             q=search, 
-            language='en'
+            sort_by='relevancy',
+            language='en', 
         )
 
         #limit amount of articles available 
